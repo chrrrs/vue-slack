c9fb5722294a834caa8fa924b9632037455d3406
Chris <chriseckert.ce@gmail.com>
2018-03-11 18:09:32 +0100

    29m 35s

--- src/components/messages/Messages.vue
+++ src/components/messages/Messages.vue
@@ -1,14 +1,87 @@
 <template>
-  <div>
-    messages
+  <div class="messages__container">
+    <div class="messages__content">
+      <h2 class="ui inverted center aligned header"> Current Channel</h2>
+      <div class="ui segment">
+        <div class="ui comments">
+          <!-- Single messages -->
+          <!-- <span v-for="message in messages"></span> -->
+          <single-message :class="message" v-for="message in messages"></single-message>
+        </div>
+      </div>
+    </div>
+
+    <!-- message form -->
+    <message-form></message-form>
+
   </div>
 </template>
 
 <script>
+  import MessageForm from './MessageForm'
+  import SingleMessage from './SingleMessage'
+  import {mapGetters} from 'vuex'
+
   export default {
     name: 'Messages',
+    components: { MessageForm, SingleMessage },
+    data() {
+      return {
+        messagesRef: firebase.database().ref('messages'),
+        messages: [],
+        channel: null
+      }
+    },
+    computed: {
+      ...mapGetters(['currentChannel', 'currentUser'])
+    },
+    watch: {
+      currentChannel() {
+        this.messages = []
+
+        this.detachListeners()
+
+        this.addListeners()
+
+        this.channe = this.currentChannel
+      }
+    },
+    methods: {
+      addListeners() {
+        this.messagesRef.child(this.currentChannel.id).on('child_added', snap => {
+          this.messages.push(snap.val())
+        })
+      },
+      detachListeners() {
+        if(this.channel !== null) {
+          this.messagesRef.child(this.channel.id).off('child_added')
+        }
+      }
+    },
+    beforeDestroy() {
+      this.detachListeners()
+    }
   }
 </script>
 
 <style scoped>
+
+  .messages__container{
+    position: relative;
+    background-color: #2a2a2e;
+    padding: 10px 30px 210px 30px;
+    margin-left: 300px;
+    min-height: 100vh;
+  }
+  .messages__container .comments{
+    font-size: 1.2em;
+  }
+  .list-enter-active{
+    transition: all 0.3s
+  }
+  .list-enter, .list-leave-to{
+    opacity: 0;
+    transform: translateX(30px);
+  }
+
 </style>
---
+++ src/components/messages/SingleMessage.vue
@@ -0,0 +1,38 @@
+<template >
+  <div class="comment comment__container">
+    <a href="avatar">
+      <img :src="message.user.avatar"/>
+    </a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'single-message',
+  props: ['message'],
+  mounted() {
+    console.log(this.message)
+  }
+}
+
+</script>
+
+<style scoped>
+  .comment__container pre{
+   font-size: 0.9em;
+   background: #232323;
+   box-shadow: 3px 6px 9px 0px #a2a2a2;
+  }
+  .comment__self{
+   border-left: 10px solid orange;
+   padding-left: 8px;
+  }
+  .comment__image{
+   min-height: 100px;
+   margin-top: 16px;
+  }
+  .comment__date{
+   color: #767676;
+  }
+</style>
